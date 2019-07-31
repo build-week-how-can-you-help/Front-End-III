43b22c6b7940f2b0c35b64de7823296442a1004c
Scott Renz <scottrenz@scottrenz.com>
2019-07-30 20:08:27 -0400

    saw that on laptop not aligning as on mac and changed formating

--- src/App 2.js
+++ src/App 2.js
@@ -2,13 +2,7 @@ import React, { useState } from "react"
 import Form from './Form'
 
 function App() {
-  // const [member, setMember] = useState(props.memberToEdit || {name: '', email: '', role: ''})
-  // const [member, setMember] = useState({name: [], email: [], role: []})
   const [kind, setKind] = useState([])
-  // const [member, setMember] = useState({name: '', email: '', role: ''})
-  // const [member, setMember] = useState('why')
-
-/* li {display: inline;}  */
 
 const ul = {
   float: 'left', 
